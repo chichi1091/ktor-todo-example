06ae2c4e1c09377307acfeec4aa9458b0063726f
Chiharu Terashima <chichi.pa2.1091@gmail.com>
2020-08-11 17:09:03 +0900

    first commit

---
+++ src/Application.kt
@@ -0,0 +1,21 @@
+package com.todo.exmaple
+
+import io.ktor.application.*
+import io.ktor.http.ContentType
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.tomcat.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("Hello World!", ContentType.Text.Plain)
+        }
+    }
+}
+
