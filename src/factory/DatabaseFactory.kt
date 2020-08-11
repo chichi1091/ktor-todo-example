e0a38b771b967a48203717ef70625f934e53ff73
Chiharu Terashima <chichi.pa2.1091@gmail.com>
2020-08-11 23:08:28 +0900

    DB接続

---
+++ src/factory/DatabaseFactory.kt
@@ -0,0 +1,31 @@
+package com.todo.exmaple.factory
+
+import com.typesafe.config.ConfigFactory
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.ktor.config.HoconApplicationConfig
+import org.jetbrains.exposed.sql.Database
+
+class DatabaseFactory {
+    private val appConfig = HoconApplicationConfig(ConfigFactory.load())
+    private val dbUrl = appConfig.property("db.jdbcUrl").getString()
+    private val dbUser = appConfig.property("db.dbUser").getString()
+    private val dbPassword = appConfig.property("db.dbPassword").getString()
+
+    fun init() {
+        Database.connect(hikari())
+    }
+
+    private fun hikari(): HikariDataSource {
+        val config = HikariConfig()
+        config.driverClassName = "org.postgresql.Driver"
+        config.jdbcUrl = dbUrl
+        config.username = dbUser
+        config.password = dbPassword
+        config.maximumPoolSize = 3
+        config.isAutoCommit = false
+        config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        config.validate()
+        return HikariDataSource(config)
+    }
+}
